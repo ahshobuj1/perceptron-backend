9faa6b95a50c0fe86499751b5c2bdec503b4cf41
A H Shobuj <ahshobuj09@gmail.com>
2025-11-01 20:39:45 +0600

    create super admin

---
+++ src/app/DB/index.ts
@@ -0,0 +1,23 @@
+import config from '../config';
+import { UserRole } from '../modules/Auth/auth.interface';
+import { UserModel } from '../modules/Auth/auth.model';
+
+const superUser = {
+  email: 'ahsobuj@gmail.com',
+  password: config.super_admin_pass,
+  role: UserRole.super_admin,
+  status: 'in-progress',
+  isDeleted: false,
+};
+
+const seedSuperAdmin = async () => {
+  const isSuperAdminExist = await UserModel.findOne({
+    role: UserRole.super_admin,
+  });
+
+  if (!isSuperAdminExist) {
+    await UserModel.create(superUser);
+  }
+};
+
+export default seedSuperAdmin;
